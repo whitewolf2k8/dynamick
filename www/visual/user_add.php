c7685eeaba0e89d7be9d22d4f293fa223e0f5c42
aleks <white_wolf_2k8@mail.ru>
2016-12-07 15:57:43 +0200

    Add  block add user and model table

--- www/visual/user_add.php
+++ www/visual/user_add.php
@@ -11,15 +11,18 @@
   <link href="../css/menu.css" rel="stylesheet">
 	<link href="../css/messege.css" rel="stylesheet">
 	<link href="../css/digitForm.css" rel="stylesheet">
+	<link href="../css/table.css" rel="stylesheet">
 
   <script type="text/javascript" src="../javascriptliblibrary/jquery.min.js"></script>
 	<script type="text/javascript" src="../jsscript/popup.js"></script>
 	<script type="text/javascript" src="../jsscript/function.js"></script>
 
+	<script type='text/javascript' src="../snow/snow.js"></script>
+
 </head>
 
-<body onload="digitalWatch()" >
-
+<body onload="digitalWatch();snow(1);" >
+<div id="snow">	</div>
 <div class="wrapper">
 	<header class="header">
 		<? require_once("header.php"); ?>
@@ -27,6 +30,102 @@
 
 	<main class="content">
 		<h2 style="	text-align: center;"> Довідник користувачів системи </h2>
+		<div class="item_blue" style="float:left; margin-left:60px; width:310px;" >
+			<h2>Додати користувача</h2>
+			<p>
+				<div class="navigation_left" id='label'>Ім&#8242;я</div>
+				<div class="navigation_right" ><input type="text" style="width:250px;"></div>
+			</p>
+			<div class="clr"></div>
+			<p>
+				<div class="navigation_left" id='label'>Логін</div>
+				<div class="navigation_right" ><input type="text" style="width:250px;"></div>
+			</p>
+			<div class="clr"></div>
+			<p>
+				<div class="navigation_left" id='label'>Пароль</div>
+				<div class="navigation_right" ><input type="text" style="width:250px;"></div>
+			</p>
+			<div class="clr"></div>
+			<p>
+				<div class="navigation_left" id='label'>Відділ</div>
+				<div class="navigation_right" ><select style="width:250px;"></select></div>
+			</p>
+
+			<p style="text-align:center;" >
+				<input type="button" value="Збрегти" >
+				<input type="button" value="Очистити" >
+			</p>
+		</div>
+
+		<div class="item_blue" style="float:right; margin-right:60px; width:310px;" >
+			<h2>Додати користувача</h2>
+			<p>
+				<div class="navigation_left" id='label'>Ім&#8242;я</div>
+				<div class="navigation_right" ><input type="text" style="width:250px;"></div>
+			</p>
+			<div class="clr"></div>
+			<p>
+				<div class="navigation_left" id='label'>Логін</div>
+				<div class="navigation_right" ><input type="text" style="width:250px;"></div>
+			</p>
+			<div class="clr"></div>
+			<p>
+				<div class="navigation_left" id='label'>Пароль</div>
+				<div class="navigation_right" ><input type="text" style="width:250px;"></div>
+			</p>
+			<div class="clr"></div>
+			<p>
+				<div class="navigation_left" id='label'>Відділ</div>
+				<div class="navigation_right" ><select style="width:250px;"></select></div>
+			</p>
+
+			<p style="text-align:center;" >
+				<input type="button" value="Збрегти" >
+				<input type="button" value="Очистити" >
+			</p>
+		</div>
+		 <div class="clr"></div >
+  <div align="center">
+		<table>
+<tr>
+  <th>Company</th>
+  <th>Q1</th>
+  <th>Q2</th>
+  <th>Q3</th>
+  <th>Q4</th>
+  </tr>
+ <tr>
+  <td>Microsoft</td>
+  <td>20.3</td>
+  <td>30.5</td>
+  <td>23.5</td>
+  <td>40.3</td>
+ </tr>
+<tr>
+  <td>Google</td>
+  <td>50.2</td>
+  <td>40.63</td>
+  <td>45.23</td>
+  <td>39.3</td>
+</tr>
+<tr>
+  <td>Apple</td>
+  <td>25.4</td>
+  <td>30.2</td>
+  <td>33.3</td>
+  <td>36.7</td>
+</tr>
+<tr>
+  <td>IBM</td>
+  <td>20.4</td>
+  <td>15.6</td>
+  <td>22.3</td>
+  <td>29.3</td>
+</tr>
+</table>
+</div>
+
 	</main><!-- .content -->
 	<div class="popup" id="loginForm" >
 		<div class="popup_bg"></div>
